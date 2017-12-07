7b084dbd6123776db0eb12dcdf0a327d3e846ba5
zlil <zlil.korman@gmail.com>
2017-12-08 00:33:31 +0200

    * create navigation side bar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,13 +2,16 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItmes from '../NavigiationItems/NavigiationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
 
 const toolbar = (props) => (
 
     <header className={classes.Toolbar}>
-        <div>MENU</div>
-        <Logo/>
-        <nav>
+        <DrawerToggle clicked={props.drawerToggleClicked}></DrawerToggle>
+        <div className={classes.Logo}>
+            <Logo/>
+        </div>
+        <nav className={classes.DesktopOnly}>
             <NavigationItmes/>
         </nav>
     </header>
